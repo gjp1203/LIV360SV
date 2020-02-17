5fd318393155f2b16fa87e8c7caff7d999a1e39e
Greg <g.j.palmer@student.liverpool.ac.uk>
2020-02-17 09:05:24 +0000

    Added preprocessing script

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # identify_unhealthy_advertisements
+
+WorkFlow components:
+
+1.) Process images using seamseg
+2.) Extract individual ads and areas using pre-processing scritp
+3.) Get STN parameters for each ad
+4.) Transform each ad into
+5.) Classify
+ 
---
+++ preprocess.py
@@ -0,0 +1,71 @@
+from PIL import Image
+import numpy as np
+import scipy.misc
+import torch
+import glob
+import os
+import cv2
+
+def crop_image(image):
+    mask = image > 0
+
+    # Coordinates of non-black pixels.
+    coords = np.argwhere(mask)
+
+    # Bounding box of non-black pixels.
+    x0, y0, _  = coords.min(axis=0)
+    x1, y1, _  = coords.max(axis=0) + 1   # slices are exclusive at the top
+
+    # Get the contents of the bounding box.
+    return image[x0:x1, y0:y1]
+
+
+segmentations = "seamseg_outputs"
+originals = "originals"
+files = glob.glob("./"+segmentations+"/*.pth.tar")
+print(files)
+for file in files: 
+    labels = torch.load(file)['sem_pred'].cpu()
+    image = np.zeros((labels.shape[0], labels.shape[1], 3), dtype=np.uint8)
+    image[labels == 39] = [40, 40, 40]
+    mask = np.zeros_like(image)
+    mask[np.all(image == [40,40,40], axis=-1)] = 255
+
+    #find all connected components (white blobs in your image)
+    nb_components, output, stats, centroids = cv2.connectedComponentsWithStats(mask[:,:,0].astype(np.uint8), connectivity=8)
+
+    #connectedComponentswithStats yields every seperated component with information on each of them, such as size
+    #the following part is just taking out the background which is also considered a component, but most of the time we don't want that.
+    sizes = stats[1:, -1]; nb_components = nb_components - 1
+
+    # minimum size of particles we want to keep (number of pixels)
+    #here, it's a fixed value, but you can set it as you want, eg the mean of the sizes or whatever
+    min_size = 1000  
+
+    #your answer image
+    #for every component in the image, you keep it only if it's above min_size
+    for i in range(0, nb_components):
+        if sizes[i] >= min_size:
+           img2 = np.zeros((output.shape))
+           img2[output == i + 1] = 255
+
+           # Mask
+           maskname = file.replace(".pth.tar", "_"+str(i)+"mask.png")
+           cv2.imwrite(maskname, img2) # Expensive step that should be removed.
+           im = cv2.imread(maskname) 
+           imgray = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
+           ret, thresh = cv2.threshold(imgray, 127, 255, 0)
+           contours, hierarchy = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+
+           hull = cv2.convexHull(contours[0])
+           cv2.fillPoly(im, pts =[hull], color=(255,255,255))
+           wContours = cv2.drawContours(im, [hull], 0, (0,255,0), 3)
+           cv2.imwrite(maskname, crop_image(wContours))
+
+           # Masked Image
+           print(file.replace(".pth.tar", ".jpg").replace('seamseg_output', 'original'))
+           original = cv2.imread(file.replace(".pth.tar", ".jpg").replace('seamseg_output', 'original'))
+           original = cv2.resize(original, (2048,1024))
+           original[(img2 != 255)] = 0.0
+           cv2.imwrite(file.replace(".pth.tar", '_'+str(i)+'masked.png'), crop_image(original))
+
